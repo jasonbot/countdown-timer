53a0339cfa5e83c6d36d65783394ecf4ff53b928
Jason Scheirer <jason.scheirer@gmail.com>
2014-01-01 13:02:43 -0800

    Move code around why not

--- countdown.py
+++ countdown.py
@@ -12,7 +12,7 @@ class CountdownTimer(object):
         if now.month < 2:
             year = now.year
         self.nye = datetime.datetime(year, 1, 1, 0, 0, 0)
-        print self.nye
+    def startup(self):
         pygame.init()
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.canvas = pygame.Surface(self.screen.get_size())
@@ -79,6 +79,7 @@ class CountdownTimer(object):
                 (item.type == pygame.KEYUP and item.key == pygame.K_ESCAPE)):
                 raise Exception("BYE")
     def loop(self):
+        self.startup()
         try:
             while not timer.done:
                 self.tick()
